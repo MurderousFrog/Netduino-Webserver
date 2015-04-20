b138088cd573b97fa1b8bb91a77a395f67031dfa
MurderousFrog <nikolaus.mutewsky@gmx.at>
2015-04-20 12:37:40 +0200

    HTTPRequest function

---
+++ Webserver/HTTPHeader.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Webserver
+{
+    class HTTPHeader
+    {
+        public HTTPHeader()
+        {
+
+        }
+        /// <summary>
+        /// Add a header field to the HTTP Header
+        /// </summary>
+        /// <param name="header"></param>
+        /// <param name="value"></param>
+        public void Add(string field, string value){
+
+        }
+    }
+}
