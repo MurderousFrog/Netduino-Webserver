6129d1be6cbc7e4edf874be0a5a0b584c64813ca
MurderousFrog <nikolaus.mutewsky@gmx.at>
2015-04-20 22:30:29 +0200

    Added some error and null checking

--- Webserver/Extensions.cs
+++ Webserver/Extensions.cs
@@ -17,15 +17,24 @@ namespace Webserver
         }
         public static string Replace(this String str, String oldValue, String newValue)
         {
+            if (str == "" || str == null) { return null; }
             StringBuilder stringbuilder = new StringBuilder(str);
-            stringbuilder.Replace(oldValue, newValue);
+            if (str.IndexOf(oldValue) > -1)
+            {
+                stringbuilder.Replace(oldValue, newValue);                
+                
+            }
             return stringbuilder.ToString();
         }
         public static string Replace(this String str, Char oldChar, Char newChar)
         {
+            if (str == "" || str == null) { return null; }
             StringBuilder stringbuilder = new StringBuilder(str);
-            stringbuilder.Replace(oldChar, newChar);
+            if (str.IndexOf(oldChar) > -1)
+            {
+                stringbuilder.Replace(oldChar, newChar);                
+            }
             return stringbuilder.ToString();
         }
-    } 
+    }
 }
