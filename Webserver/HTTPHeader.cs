a0f4a4c9a70add44e70795cb3b56391017de0f0c
MurderousFrog <nikolaus.mutewsky@gmx.at>
2015-04-20 22:31:33 +0200

    Added a static Parse Method

--- Webserver/HTTPHeader.cs
+++ Webserver/HTTPHeader.cs
@@ -21,25 +21,61 @@ namespace Webserver
         /// <summary>
         /// Add a header field to the HTTP Header
         /// </summary>
-        /// <param name="header"></param>
-        /// <param name="value"></param>
-        public void Add(string key, string value){
-            Pair p = new Pair(key, value);
+        /// <param name="header">Header name</param>
+        /// <param name="value">Header value</param>
+        public void Add(string header, string value){
+            Pair p = new Pair(header, value);
             list.Add(p);
         }
-        public void Remove(string id)
+        /// <summary>
+        /// Remove a header from list
+        /// </summary>
+        /// <param name="header">Header name</param>
+        public void Remove(string header)
         {
-            list.Remove(GetObject(id));
+            list.Remove(GetObject(header));
         }
-        public string ToString()
+        public override string ToString()
         {
-            //TODO
             StringBuilder s = new StringBuilder();
             foreach (Pair p in list)
             {
-                s.AppendLine(p.Key() + ": " + p.Value() + "\n");
+                s.Append(p.Key() + ": " + p.Value() + "\r\n");
             }
+            s.Append("\r\n");
             return s.ToString();
+        }
+        public static HTTPHeader Parse(string rawHeader){
+            if (rawHeader == null) { return null; }
+            HTTPHeader header = new HTTPHeader();
+            try
+            {
+                //Split raw data into lines
+                string[] lines = rawHeader.Split(new char[] { '\n' });
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    //Remove line breaks and carriage returns
+                    lines[i] = lines[i].Replace("\n", "").Replace("\r", "");
+
+                    if (lines[i] != null && lines[i].IndexOf(':') > -1)
+                    {
+                        //Split at all ':', first substring is the header name
+                        string headerName = lines[i].Split(new char[] { ':' })[0];
+                        //Substring after ':' is headerValue
+                        string headerValue = lines[i].Substring(lines[i].IndexOf(':') + 2);
+
+                        //Add header to list
+                        header.Add(headerName, headerValue);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                DebugUtils.Print(DebugLevel.ERROR, "Failed to parse raw data into HTTP header.");
+                DebugUtils.Print(DebugLevel.ERROR, e.StackTrace);
+            }
+            return header;
         }
         private Object GetObject(string id)
         {
@@ -50,7 +86,7 @@ namespace Webserver
             return null;
         }
     }
-    private class Pair
+    class Pair
     {
         string key;
         string value;
